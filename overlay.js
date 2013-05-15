3023d5d66db278bf1fceb5ec1998d30673e54f56
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-05-15 18:56:40 +0200

    loading one hardcoded miid

--- overlay.js
+++ overlay.js
@@ -5,6 +5,13 @@ var Events = require('github/jillix/events');
 
 module.exports = function (config) {
 
+    config.classes = {
+        close: '',
+        ok: '',
+        cancel: '',
+        body: ''
+    };
+
     var self = this;
 
     // process the event configuration
@@ -13,7 +20,25 @@ module.exports = function (config) {
     this.show = function(event, miid) {
         $('.modal', self.dom).modal();
 
-        M($('.modal-body', seld.dom), 'content');
+        var body = $('.overlay-body', self.dom);
+        var waiter = $('.overlay-waiter', self.dom);
+
+        // show busy body
+        body.hide();
+        waiter.show();
+
+        // only if we find the body
+        if (body.get(0)) {
+            // first empty the body (from previous load)
+            body.empty();
+            // start loading the module
+            setTimeout(function() {
+                M(body.get(0), 'content', function() {
+                    waiter.hide();
+                    body.show();
+                });
+            }, 2000);
+        }
     };
 };
 
