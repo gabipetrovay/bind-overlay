e4cab3228fdb6a9e8afa465b1b25dbf51bb9b9f9
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-05-16 12:50:29 +0200

    Added default configuration and the button logic

--- overlay.js
+++ overlay.js
@@ -5,27 +5,34 @@ var Events = require('github/jillix/events');
 
 module.exports = function (config) {
 
-    config.classes = {
-        close: '',
-        ok: '',
-        cancel: '',
-        body: ''
-    };
-
+    var config = processConfig(config);
     var self = this;
 
     // process the event configuration
-    Events.call(this, config);
+    Events.call(self, config);
 
-    this.show = function(event, miid) {
-        $('.modal', self.dom).modal();
+    self.show = function(event, miid) {
 
-        var body = $('.overlay-body', self.dom);
-        var waiter = $('.overlay-waiter', self.dom);
+        var body = $('.' + config.classes.body, self.dom);
+        var waiter = $('.' + config.classes.waiter, self.dom);
+        var actionButtons = $('.' + config.classes.yes + ', .' + config.classes.no, self.dom);
+        var allButtons = $('.' + config.classes.yes + ', .' + config.classes.no + ', .' + config.classes.cancel, self.dom);
+
+        // the texts
+        for (var i in config.texts) {
+            $('.' + config.classes[i], self.dom).text(config.texts[i]);
+        }
+
+        allButtons.hide();
+
+        for (var i in config.buttons) {
+            $('.' + config.classes[config.buttons[i]], self.dom).show();
+        }
 
         // show busy body
         body.hide();
         waiter.show();
+        actionButtons.addClass(config.classes.disabled);
 
         // only if we find the body
         if (body.get(0)) {
@@ -36,9 +43,82 @@ module.exports = function (config) {
                 M(body.get(0), 'content', function() {
                     waiter.hide();
                     body.show();
+                    actionButtons.removeClass(config.classes.disabled);
                 });
             }, 2000);
         }
+
+        // now show the overlay modal
+        $('.modal', self.dom).modal();
     };
+
+    self.hide = function(event) {
+        $('.modal', self.dom).modal('hide');
+    };
+
+    self.yes = function() {
+        self.hide();
+        self.emit('yes');
+    };
+
+    self.no = function() {
+        self.hide();
+        self.emit('no');
+    };
+
+    self.cancel = function() {
+        self.hide();
+        self.emit('cancel');
+    };
+
+    // add handlers for each button
+    for (var i in config.buttons) {
+        if (self[config.buttons[i]]) {
+            (function(i) {
+                $('.' + config.classes[config.buttons[i]], self.dom).on('click', function() {
+                    if (!$(this).hasClass('disabled')) {
+                        self[config.buttons[i]]();
+                    }
+                    return false;
+                });
+            })(i);
+        }
+    }
 };
+
+function processConfig(config) {
+
+    // defaults
+    var defaultClasses = {
+        title: 'overlay-title',
+        body: 'overlay-body',
+        waiter: 'overlay-waiter',
+        yes: 'overlay-yes',
+        no: 'overlay-no',
+        cancel: 'overlay-cancel',
+        buttons: 'overlay-buttons',
+        disabled: 'disabled'
+    };
+    var defaultButtons = [ 'yes', 'no', 'cancel' ];
+    var defaultTexts = {
+        title: '',
+        yes: 'Yes',
+        no: 'No',
+        cancel: 'Cancel'
+    };
+
+    config.classes = config.classes || {};
+    for (var i in defaultClasses) {
+        config.classes[i] = config.classes[i] || defaultClasses[i];
+    }
+
+    config.buttons = config.buttons || defaultButtons;
+
+    config.texts = config.texts || defaultTexts;
+    for (var i in defaultTexts) {
+        config.texts[i] = config.texts[i] || defaultTexts[i];
+    }
+
+    return config;
+}
 
